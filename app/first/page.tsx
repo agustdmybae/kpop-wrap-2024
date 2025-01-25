2e86257a1ac71a3f1d0ebe2197062f4cf64ec8c5
Chi Chen <stephchen55@gmail.com>
2025-01-25 23:15:57 +0000

    Add sticky position

--- app/first/page.tsx
+++ app/first/page.tsx
@@ -31,8 +31,8 @@ export default function First() {
     </div>
   )
     return (
-      <div>
-        <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className='relative h-[200vh]'>
+        <div className="h-screen sticky top-0 flex flex-col items-center justify-center min-h-screen">
           <motion.a 
             style={{overflow:"hidden", whiteSpace:"nowrap", textAlign: "center"}}
             initial={{width:0}}
@@ -41,7 +41,7 @@ export default function First() {
             className="text-yellow-300 text-4xl font-bold"
           >More than 300 Kpop albums are released in the year.</motion.a>
         </div>
-        <div className="bg-yellow-300 flex flex-col gap-6 items-center justify-center min-h-screen">
+        <div className="relative h-screen bg-yellow-300 flex flex-col gap-6 items-center justify-center">
           <a className="text-black text-4xl font-bold">Among all the masterpieces, these albums stand out.</a>
           <a className="text-black text-2xl font-medium">Do you spot your favorite one?</a>
           <div className="my-3 bg-yellow-100 text-black rounded-lg p-8">
