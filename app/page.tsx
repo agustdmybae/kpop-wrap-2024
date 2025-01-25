387007f2340bb8ee1fa7b3566f43969596c335b1
Chi Chen <stephchen55@gmail.com>
2025-01-25 21:14:29 +0000

    Add fade-in effect on the first landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+'use client';
+import { motion } from 'framer-motion';
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center justify-center min-h-screen gap-6">
-        <a className="text-yellow-300 text-7xl font-bold">2024 KPOP Wrapped.</a>
-        <div className="flex flex-col justify-center items-center">
-          <a className="text-yellow-300 text-2xl">Another epic year of KPOP</a>
-          <a className="text-yellow-300 text-2xl">Let’s rewind on the amazing moments</a>
-        </div>
-        <Link className='border-solid border-2 rounded border-yellow-300 w-20 h-8 text-center text-yellow-300 text-2xl' href="/first">→</Link>
+        <motion.a 
+          className="text-yellow-300 text-7xl font-bold"
+          initial={{ opacity: 0, y: 50}}
+          animate={{ opacity: 1, y: 0 }} 
+          transition={{duration:0.7}}
+        >2024 KPOP Wrapped.</motion.a>
+        <motion.div
+          initial={{ opacity: 0, y: 50}}
+          animate={{ opacity: 1, y: 0 }} 
+          transition={{duration:0.7, delay:1.5}}
+          className="flex flex-col items-center justify-center gap-5"
+        >
+          <div className="flex flex-col justify-center items-center">
+            <a className="text-yellow-300 text-2xl">Another epic year of KPOP</a>
+            <a className="text-yellow-300 text-2xl">Let’s rewind on the amazing moments</a>
+          </div>
+          <Link className='border-solid border-2 rounded border-yellow-300 w-20 h-8 text-center text-yellow-300 text-2xl' href="/first">→</Link>
+        </motion.div>
       </main>
       {/* 
       <div className="font-[family-name:var(--font-geist-sans)] my-52">
