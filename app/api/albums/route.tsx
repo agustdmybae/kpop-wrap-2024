7ea43b9ab43b1b938ebc7bff3fee8bf437fca48b
Chi Chen <stephchen55@gmail.com>
2025-02-07 21:18:31 -0500

    Update data fetch limit to 5

--- app/api/albums/route.tsx
+++ app/api/albums/route.tsx
@@ -6,6 +6,6 @@ export async function GET() {
   const client = await connectToDatabase();
 
   const topAlbumCollections = client.db("kpop").collection("topAlbums");
-  const topAlbums = await topAlbumCollections.find({}).toArray();
+  const topAlbums = await topAlbumCollections.find({}).limit(5).toArray();
   return NextResponse.json(topAlbums);
 }
--- app/second/page.tsx
+++ app/second/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import useSWR from 'swr'
+import { motion } from 'framer-motion';
 
 const fetcher = (url: string) => fetch(`/api${url}`).then((res) => res.json());
 
@@ -19,23 +20,28 @@ export default function Second() {
   }
   
   const songList = data.map((song: SongData, index: number)=>
-    <div key={index} className="grid grid-cols-2 gap-20 text-lg pb-2">
+    <div key={index} className="grid grid-cols-2 gap-20 text-lg pb-2 text-black">
       <div>{index+1}. {song.songTitle}</div>
       <div>{song.artist}</div>
     </div>
   )
     return (
-      <div>
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <a className="text-yellow-300 text-4xl font-bold">Of course... there are many hits as well</a>
+      <div className='relative h-[200vh]'>
+        <div className="h-screen sticky top-0 flex flex-col items-center justify-center min-h-screen">
+          <motion.a 
+            className="text-yellow-300 text-4xl font-bold"
+            initial={{ opacity: 0, y: 50}}
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{duration:0.7}}
+          >Of course... there are many hits as well</motion.a>
         </div>
-        <div className="flex flex-col gap-6 items-center justify-center min-h-screen">
-          <a className="text-yellow-300 text-4xl font-bold">These are the Top 10 Streamed Songs</a>
+        <div className="relative h-screen bg-yellow-300 flex flex-col gap-6 items-center justify-center min-h-screen">
+          <a className="text-black text-4xl font-bold">These are the Top 10 Streamed Songs</a>
           <div className="mt-8">
             {songList}
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <div className="relative h-screen flex flex-col items-center justify-center min-h-screen gap-4">
           <a className="text-yellow-300 text-4xl font-bold">Can you guess which song</a>
           <a className="text-yellow-300 text-4xl font-bold">won the most music show awards?</a>
         </div>
